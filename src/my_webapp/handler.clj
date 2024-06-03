15300946eef41c85401ad1c425d61d3fab2dd507
Artem Kulakov <artem.kulakov@gmail.com>
2024-06-03 08:47:50 +0300

    Working auth on home page

--- src/my_webapp/handler.clj
+++ src/my_webapp/handler.clj
@@ -1,16 +1,29 @@
 ;; src/my_webapp/handler.clj
 (ns my-webapp.handler
-  (:require [compojure.core :refer [defroutes GET POST]] ; add POST here
+  (:require [compojure.core :refer [defroutes GET POST]]
             [compojure.route :as route]
-            [my-webapp.views :as views] ; add this require
+            [my-webapp.views :as views]
             [ring.adapter.jetty :as jetty]
             [ring.middleware.reload :refer [wrap-reload]]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [compojure.response :refer [render]]
+            [clojure.java.io :as io]
+            [ring.util.response :refer [response redirect content-type]]
+            [buddy.auth :refer [authenticated? throw-unauthorized]]
+            [buddy.auth.backends.httpbasic :refer [http-basic-backend]]
+            [buddy.auth.middleware :refer [wrap-authentication wrap-authorization]]
+            ))
 
-(defroutes app-routes ; replace the previous app-routes with this
+(defn home
+  [req]
+  (if-not (authenticated? req)
+    (throw-unauthorized)
+    (views/home-page)))
+
+(defroutes app-routes
   (GET "/"
     []
-    (views/home-page))
+    home)
   (GET "/add-location"
     []
     (views/add-location-page))
@@ -24,10 +37,27 @@
     []
     (views/all-locations-page))
   (route/resources "/")
-  (route/not-found "Not Foundx"))
+  (route/not-found "Not Found"))
 
+(def authdata
+  {:admin "secret"
+   :test "secret"})
+
+(defn my-authfn
+  [req {:keys [username password]}]
+  (when-let [user-password (get authdata (keyword username))]
+    (when (= password user-password)
+      (keyword username))))
+
+(def auth-backend
+  (http-basic-backend {:realm "MyExampleSite"
+                       :authfn my-authfn}))
 (def app
   (wrap-reload (wrap-defaults #'app-routes site-defaults)))
 
-(defn -main []
-  (jetty/run-jetty #'app {:port 3000}))
+(defn -main
+  [& args]
+  (as-> app $
+    (wrap-authorization $ auth-backend)
+    (wrap-authentication $ auth-backend)
+    (jetty/run-jetty $ {:port 3000})))
