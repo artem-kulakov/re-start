c21cf7b33f90fa18b8e7e5d30a00c78455a31908
Artem Kulakov <artem.kulakov@gmail.com>
2024-06-25 12:35:19 +0300

    Migrate to Bootstrap

--- src/my_webapp/views.clj
+++ src/my_webapp/views.clj
@@ -3,112 +3,118 @@
             [ring.util.anti-forgery :as util]))
 
 (defn page-head
-  [title]
+  ([title styles]
     [:head
-      [:meta {:charset "utf-8"}]
-      [:meta {:name "viewport", :content "width=device-width, initial-scale=1"}]
-      [:title title]
-      [:link {:rel "manifest", :href "/manifest.webmanifest"}]
-      [:script {:src "https://unpkg.com/htmx.org@1.9.12"}]
-      (page/include-css "/css/bulma.min.css")
-      (page/include-css "/css/styles.css")])
+     [:meta {:charset "utf-8"}]
+     [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
+     [:title title]
+     [:link
+      {:href
+       "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+       :rel "stylesheet"
+       :integrity
+       "sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+       :crossorigin "anonymous"}]
+     (page/include-css (str "/css/" styles ".css"))
+     [:script
+      {:src
+       "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+       :integrity
+       "sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+       :crossorigin "anonymous"}]])
+  ([title] (page-head title "styles")))
 
-(def nav
-  [:div#nav
-   [:a {:href "/lists"} "My lists"]
-   " | "
-   [:a {:href "/logout"} "Logout"]])
+(defn nav
+  ([page]
+  [:div.row.my-2
+   [:div.col
+    [:div.btn-group
+      [:a.btn.btn-light {:href "/lists" :class (when (= page "lists") "disabled")} "Lists"]
+      [:a.btn.btn-light {:href "/logout"} "Logout"]]]])
+  ([] (nav "")))
 
-(defn lists
-  [lists]
-  (page/html5
-   (page-head "My lists")
-   nav
-    [:body
-    [:section {:class "section"}
-      [:div {:class "container"}
-      [:div {:class "columns"}
-        [:div {:class "column is-narrow"}
-        [:form {:method "POST", :action "/add-list"}
-          [:div {:class "field has-addons"}
-          [:div {:class "control"}
-            (util/anti-forgery-field)
-            [:input
-            {:class "input",
-              :type "text",
-              :name "name",
-              :value "",
-              :placeholder "Enter the list name"}]]
-          [:div {:class "control"}
-            [:input {:type "submit", :class "button is-link", :value "Add"}]]]]]]
-      [:div {:class "columns"}
-        [:div {:class "column is-narrow"}
-        (for [list lists]
-          [:div {:class "is-flex is-justify-content-space-between"}
-            [:div {:class "pb-4 pr-6"}
-             [:p [:a {:href (str "lists/" (:id list))} (:name list)]]]])]]]]]))
+(def colors
+  ["blue" "yellow" "indigo" "green" "red" "teal" "orange" "cyan" "pink"])
 
 (defn list-page
   [list items]
   (page/html5
-   (page-head name)
-   nav
-   [:body
-    [:section {:class "section"}
-     [:div {:class "container"}
-      [:div {:class "columns"}
-       [:div {:class "column is-narrow"}
-        [:h1 (:name list)]
-        ]]
-      [:div {:class "columns"}
-       [:div {:class "column is-narrow"}
-        [:form {:method "POST", :action (str "/lists/" (:id list) "/add-item")}
-         [:div {:class "field has-addons"}
-          [:div {:class "control"}
-           (util/anti-forgery-field)
-           [:input
-            {:class "input",
-             :type "text",
-             :name "name",
-             :value "",
-             :placeholder "Enter the product name"}]]
-          [:div {:class "control"}
-           [:input {:type "submit", :class "button is-link", :value "Add"}]]]]]
-       [:div {:class "column"}
-        [:form
-         {:method "POST", :action (str "/lists/" (:id list) "/sort-items")}
-         (util/anti-forgery-field)
-         [:input {:type "submit", :class "button is-info", :value "Sort"}]]]]
-      [:div {:class "columns"}
-        [:div {:class "column is-narrow"}
-          (for [item items]
-            [:div {:class "is-flex is-justify-content-space-between"}
-            [:div {:class "pb-4 pr-6"}
-              [:p
-              {:class (when (:complete item) "crossed-out")}
-              (:name item)]]
-            [:div
-              [:form
-              {:method "POST", :action "/toggle-item-complete"}
-              (util/anti-forgery-field)
+   (page-head (:name list))
+   [:body.bg-danger.bg-gradient
+    [:div.container
+     (nav)
+     [:div.row
+      [:div.col.d-flex.flex-row.justify-content-between
+       [:form {:method "POST", :action (str "/lists/" (:id list) "/add-item")}
+        (util/anti-forgery-field)
+        [:div.input-group.mb-2
+          [:input.form-control
+            {:type "text",
+            :name "name",
+            :value "",
+            :placeholder "Enter the product name"}]
+            [:input.btn.btn-secondary {:type "submit", :value "Add"}]]]
+       [:form
+        {:method "POST", :action (str "/lists/" (:id list) "/sort-items")}
+        (util/anti-forgery-field)
+        [:input.btn.btn-secondary {:type "submit", :value "Sort"}]]]]
+     [:div.row
+      [:div.col
+       [:ul.list-group.mb-2
+        (for [item items]
+          [:form {:method "POST", :action "/toggle-item-complete"}
+            (util/anti-forgery-field)
+            [:li.list-group-item {:onclick "this.closest('form').submit()"}
+              [:span
+                {:class
+                 (str
+                  (when (:complete item) "crossed-out")
+                  " "
+                  (nth colors (mod (:id item) 9)))}
+                (:name item)]
               [:input {:type "hidden", :name "id", :value (:id item)}]
-              [:input {:type "hidden", :name "complete", :value (str (:complete item))}]
-              [:input
-                {:type "checkbox",
-                (when (:complete item) :checked) ""
-                :onchange "this.form.submit()"}]]]])]]]]]))
+              [:input {:type "hidden", :name "complete", :value (str (:complete item))}]]])]]]]]))
+
+(defn lists
+  [lists]
+  (page/html5
+   (page-head (:name list))
+   [:body.bg-success.bg-gradient
+    [:div.container
+     (nav "lists")
+     [:div.row
+      [:div.col.d-flex.flex-row.justify-content-between
+       [:form {:method "POST", :action "/add-list"}
+        (util/anti-forgery-field)
+        [:div.input-group.mb-3
+         [:input.form-control
+          {:type "text",
+           :name "name",
+           :value "",
+           :placeholder "Enter the list name"}]
+         [:input.btn.btn-danger {:type "submit", :value "Add"}]]]]]
+     [:div.row
+      [:div.col
+       [:ul.list-group
+        (for [list lists]
+           [:li.list-group-item
+            [:a.link-underline-light.link-dark {:href (str "lists/" (:id list))} (:name list)]])]]]]]))
 
 (defn login
   []
   (page/html5
-    [:h1 "Login Page"]
-    [:form
-    {:method "post"}
-     (util/anti-forgery-field)
-    [:input {:type "text", :placeholder "Name:", :name "name"}]
-    [:input
-      {:type "password", :placeholder "Password:", :name "password"}]
-    [:input {:type "submit", :value "Submit"}]]))
+    (page-head (:name "Login") "login")
+    [:body.d-flex.align-items-center.bg-danger.bg-gradient
+     [:div.container
+      [:div.row
+       [:div.col
+        [:form
+        {:method "post"}
+          (util/anti-forgery-field)
+          [:div.input-group
+            [:input.form-control {:type "text", :placeholder "Name:", :name "name"}]
+            [:input.form-control {:type "password", :placeholder "Password:", :name "password"}]
+            [:input.btn.btn-secondary {:type "submit", :value "Sign in"}]]]]]]]))
 
 (defn error
   []
